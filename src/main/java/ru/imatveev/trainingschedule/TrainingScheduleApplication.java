2b84731a0d4f0189d1215afc581a19cc3b1c98e2
vanadiiii <vanadiiii42@gmail.com>
2021-02-09 21:58:29 +0400

    Initial commit

---
+++ src/main/java/ru/imatveev/trainingschedule/TrainingScheduleApplication.java
@@ -0,0 +1,11 @@
+package ru.imatveev.trainingschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingScheduleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingScheduleApplication.class, args);
+    }
+}
